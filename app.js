a22faf737d8a6767fd24af83466b8caba668da66
sajjad <sajjadbave9@gmail.com>
2026-08-03 02:53:46 +0330

    Update app.js

--- app.js
+++ app.js
@@ -5,38 +5,45 @@ function addAd() {
     let price = document.getElementById("price").value;
     let desc = document.getElementById("desc").value;
 
-    if (title === "" || price === "") {
-        alert("لطفاً نام کالا و قیمت را وارد کن");
+    if (title == "" || price == "") {
+        alert("نام کالا و قیمت را وارد کن");
         return;
     }
 
-    let ad = {
+    ads.push({
         title: title,
         price: price,
-        desc: desc
-    };
-
-    ads.push(ad);
+        desc: desc,
+        sold: false
+    });
 
     localStorage.setItem("ads", JSON.stringify(ads));
-
     showAds();
+}
 
-    document.getElementById("title").value = "";
-    document.getElementById("price").value = "";
-    document.getElementById("desc").value = "";
+function soldAd(index) {
+    ads[index].sold = true;
+    localStorage.setItem("ads", JSON.stringify(ads));
+    showAds();
 }
 
 function showAds() {
     let box = document.getElementById("ads");
     box.innerHTML = "";
 
-    ads.forEach(function(ad) {
+    ads.forEach(function(ad, index) {
         box.innerHTML += `
         <div>
             <h3>${ad.title}</h3>
             <p>💰 ${ad.price}</p>
             <p>${ad.desc}</p>
+
+            ${
+            ad.sold
+            ? "✅ فروخته شد"
+            : `<button onclick="soldAd(${index})">فروختم</button>`
+            }
+
             <hr>
         </div>
         `;
