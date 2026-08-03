27a48473da20ac3a62facc0c9f5ebcb7ed06db54
sajjad <sajjadbave9@gmail.com>
2026-08-03 14:07:57 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,90 @@
 let ads = JSON.parse(localStorage.getItem("ads")) || [];
 
 function addAd() {
+
     let title = document.getElementById("title").value;
     let price = document.getElementById("price").value;
     let desc = document.getElementById("desc").value;
+    let phone = document.getElementById("phone").value;
+    let city = document.getElementById("city").value;
+    let address = document.getElementById("address").value;
 
-    if (title == "" || price == "") {
+    if(title == "" || price == ""){
         alert("نام کالا و قیمت را وارد کن");
         return;
     }
 
-    ads.push({
-        title: title,
-        price: price,
-        desc: desc,
-        sold: false
-    });
+    let ad = {
+        title,
+        price,
+        desc,
+        phone,
+        city,
+        address,
+        sold:false,
+        time:new Date().toLocaleString("fa-IR")
+    };
+
+    ads.push(ad);
 
     localStorage.setItem("ads", JSON.stringify(ads));
+
     showAds();
 }
 
-function soldAd(index) {
+
+function soldAd(index){
+
     ads[index].sold = true;
+
     localStorage.setItem("ads", JSON.stringify(ads));
+
     showAds();
 }
 
-function showAds() {
+
+function showAds(){
+
     let box = document.getElementById("ads");
+
     box.innerHTML = "";
 
-    ads.forEach(function(ad, index) {
+    ads.forEach((ad,index)=>{
+
         box.innerHTML += `
-        <div>
+
+        <div class="ad">
+
             <h3>${ad.title}</h3>
-            <p>💰 ${ad.price}</p>
+
+            <p>💰 قیمت: ${ad.price}</p>
+
             <p>${ad.desc}</p>
+
+            <p>📞 ${ad.phone}</p>
+
+            <p>📍 ${ad.city}</p>
+
+            <p>🏠 ${ad.address}</p>
+
+            <p>🕒 ${ad.time}</p>
+
 
             ${
             ad.sold
-            ? "✅ فروخته شد"
+            ? "<b>✅ فروخته شد</b>"
             : `<button onclick="soldAd(${index})">فروختم</button>`
             }
 
-            <hr>
         </div>
+
+        <hr>
+
         `;
+
     });
+
 }
+
 
 showAds();
