3b0b54a276788292d6cc3a5c02e84c64689b9cdf
sajjad <sajjadbave9@gmail.com>
2026-08-04 15:22:00 +0330

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ import { db } from "./firebase.js";
 import {
   collection,
   addDoc,
-  getDocs
+  getDocs,
+  deleteDoc,
+  doc
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+
+let ads = [];
 
 
 async function addAd(){
 
   let title = document.getElementById("title").value;
-  let price = document.getElementById("price").value";
+  let price = document.getElementById("price").value;
   let desc = document.getElementById("desc").value;
   let phone = document.getElementById("phone").value;
   let city = document.getElementById("city").value;
@@ -25,12 +30,12 @@ async function addAd(){
 
   await addDoc(collection(db,"ads"),{
 
-    title:title,
-    price:price,
-    desc:desc,
-    phone:phone,
-    city:city,
-    address:address,
+    title,
+    price,
+    desc,
+    phone,
+    city,
+    address,
     sold:false,
     time:new Date().toLocaleString("fa-IR")
 
@@ -39,7 +44,112 @@ async function addAd(){
 
   alert("آگهی ثبت شد");
 
+  loadAds();
+
 }
 
 
+
+async function loadAds(){
+
+  ads=[];
+
+
+  const querySnapshot = await getDocs(collection(db,"ads"));
+
+
+  querySnapshot.forEach((item)=>{
+
+    let data=item.data();
+
+
+    ads.push({
+
+      id:item.id,
+      ...data
+
+    });
+
+  });
+
+
+  showAds();
+
+}
+
+
+
+
+async function deleteAd(id){
+
+  let answer = confirm("حذف شود؟");
+
+  if(!answer) return;
+
+
+  await deleteDoc(doc(db,"ads",id));
+
+
+  alert("آگهی حذف شد");
+
+
+  loadAds();
+
+}
+
+
+
+
+function showAds(){
+
+  let box=document.getElementById("ads");
+
+  box.innerHTML="";
+
+
+  ads.forEach((ad)=>{
+
+
+    box.innerHTML += `
+
+    <div class="ad">
+
+    <h3>${ad.title}</h3>
+
+    <p>💰 ${Number(ad.price).toLocaleString("fa-IR")} تومان</p>
+
+    <p>${ad.desc}</p>
+
+    <p>📞 ${ad.phone}</p>
+
+    <p>📍 ${ad.city}</p>
+
+    <p>🏠 ${ad.address}</p>
+
+    <p>🕒 ${ad.time}</p>
+
+
+    <button onclick="deleteAd('${ad.id}')">
+    🗑 حذف آگهی
+    </button>
+
+
+    <hr>
+
+    </div>
+
+    `;
+
+
+  });
+
+
+}
+
+
+
 window.addAd = addAd;
+window.deleteAd = deleteAd;
+
+
+loadAds();
